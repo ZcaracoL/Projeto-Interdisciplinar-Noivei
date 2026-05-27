1aaab0f0acdb3b63febc32baf9434b9ca65ad0bf
jubabdag <juvenal.yoshikawa@fatec.sp.gov.br>
2026-05-27 20:58:09 -0300

    Adicionando planos_model.js

--- Noivei_PI/back-end/models/fornecedores_model.js
+++ Noivei_PI/back-end/models/fornecedores_model.js
@@ -1,11 +1,12 @@
 import mongoose, { Schema } from "mongoose";
+import { planoSchema } from "./planos_model.js";
 
 export const fornecedorSchema = new Schema({
     nomeLoja: String,
     descricao: String,
     fotoUrl: String,
     contatoEmail: String,
-    planos: [ String ],
+    planos: planoSchema,
     criadoEm: {
         type: Date,
         default: Date.now
